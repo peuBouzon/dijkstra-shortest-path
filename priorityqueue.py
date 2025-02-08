e59235c9615eef6a89d6b0b9c05063f25b444b7e
peubouzon <peubouzon@gmail.com>
2025-02-07 23:39:36 -0300

    prioritize min elements

--- priorityqueue.py
+++ priorityqueue.py
@@ -1,50 +1,58 @@
-class PriorityQueue():
+import math
+
+class MinPriorityQueue():
     def __init__(self, max_size) -> None:
         self.max_size = max_size
         self.size = 0
-        self.heap = [None] * (max_size + 1) # start the index at 1
+        self.heap = [math.inf] * (max_size + 1) # sum one because the 0 index is not used
 
     def __len__(self):
         return self.size
-
+    
+    def __bool__(self):
+        return len(self) > 0
+    
     def push(self, value):
         self.size += 1
         self.heap[self.size] = value
-        self.swim(self.size)
+        self._swim(self.size)
 
-    def swim(self, position):
-        parent_position = position // 2
-        value_above = self.heap[parent_position]
-        current_value = self.heap[position]
-        while position > 1 and value_above < current_value:
-            self.heap[position] = value_above
-            self.heap[parent_position] = current_value
-            position = parent_position
+    def _swim(self, index):
+        while index > 1 and self._less(index, index // 2):
+            parent_index = index // 2
+            self._swap(index, parent_index)
+            index = parent_index
  
     def pop(self):
-        max = self.heap[1]
+        if not self.size:
+            return None
+        min = self.heap[1]
         self.heap[1] = self.heap[self.size]
-        self.heap[self.size] = None
+        self.heap[self.size] = math.inf
         self.size -= 1
-        self.sink(1)
-        return max
+        self._sink(1)
+        return min
 
-    def sink(self, position):
-        child_position = position * 2
-        while child_position < self.size:
+    def _sink(self, index):
+        while index * 2 <= self.size:
+            child_index = index * 2
+            # check which child has the smallest value
+            if child_index < self.size and self._less(child_index + 1, child_index): 
+                child_index += 1
 
-            # check which child has the largest value
-            if (self.heap[child_position] < self.heap[child_position + 1]): 
-                child_position += 1
-
-            # if the parent is greater than both childs, the heapify is done
-            if self.heap[position] > self.heap[child_position]:
+            # if the parent is smaller than both childs, the heapify is done
+            if self._less(index, child_index):
                 break
+            
+            self._swap(index, child_index)
+            index = child_index
 
-            temp = self.heap[position]
-            self.heap[position] = self.heap[child_position]
-            self.heap[child_position] = temp
-            position = child_position
-
+    def _less(self, i, j):
+        return self.heap[i] < self.heap[j]
+    
+    def _swap(self, i, j):
+        temp = self.heap[i]
+        self.heap[i] = self.heap[j]
+        self.heap[j] = temp
 
     
