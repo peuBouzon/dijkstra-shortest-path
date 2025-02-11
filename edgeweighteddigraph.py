3a970f931372a69a8cb1db4999aa4ca6b577c35e
peubouzon <peubouzon@gmail.com>
2025-02-11 12:02:23 -0300

    fix constructor and add typing

--- edgeweighteddigraph.py
+++ edgeweighteddigraph.py
@@ -1,16 +1,17 @@
 from bag import Bag
+from directededge import DirectedEdge
 
 class EdgeWeightedDigraph:
-    def __init__(self, n_vertices):
+    def __init__(self, n_vertices : int):
         self.n_vertices = n_vertices
         self.n_edges = 0
-        self.edges_from_vertex = Bag()
-        for _ in n_vertices:
-            self.edges_from_vertex.add(Bag())
+        self.edges_from_vertex = []
+        for _ in range(n_vertices):
+            self.edges_from_vertex.append(Bag())
 
-    def add_edge(self, edge):
+    def add_edge(self, edge : DirectedEdge):
         self.edges_from_vertex[edge.source].add(edge)
         self.n_edges += 1
 
-    def get_edges_from(self, vertex):
+    def get_edges_from(self, vertex : int):
         return self.edges_from_vertex[vertex]
