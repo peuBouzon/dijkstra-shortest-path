e4cd3185f92643e064f8cd82fd146b48a4c4c284
peubouzon <peubouzon@gmail.com>
2025-03-19 08:40:40 -0300

    rename file and print elapsed time

--- trab1.py
+++ trab1.py
@@ -5,15 +5,21 @@ import output_handler
 if __name__ == '__main__':
     import time
     import argparse
+    import timeit
     parser = argparse.ArgumentParser()
     parser.add_argument('input')
     parser.add_argument('output')
     args = parser.parse_args()
 
-    graph, source = graph_factory.from_txt(args.input)
-
     start = time.time()
+    graph, source = graph_factory.from_txt(args.input)
+    print(f'Time to build the graph: {time.time() - start}')
+
+    #elapsed = timeit.timeit(lambda : DijkstraShortestPath(graph, source), number=10)
+    #print(f'timeit: {elapsed / 10}')
+    start = time.time()
+
     shortest_paths = DijkstraShortestPath(graph, source)
-    print(f'Elapsed time: {time.time() - start}')
+    print(f'Time to find shortest paths: {time.time() - start}')
 
     output_handler.to_txt(shortest_paths, args.output)
