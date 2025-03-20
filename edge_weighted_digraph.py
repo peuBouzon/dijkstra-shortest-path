196f67714072f7b965a8acd3402c14609db3f9eb
peubouzon <peubouzon@gmail.com>
2025-03-20 15:20:52 -0300

    remove unused variable

--- edge_weighted_digraph.py
+++ edge_weighted_digraph.py
@@ -3,7 +3,6 @@ class EdgeWeightedDigraph:
     def __init__(self, n_vertices : int):
         self.n_vertices = n_vertices
         self.edges_from_vertex : List[list] = []
-        self.n_edges = 0
         for _ in range(n_vertices):
             self.edges_from_vertex.append([])
 
@@ -12,7 +11,6 @@ class EdgeWeightedDigraph:
 
     def add_edges(self, source, edges):
         self.edges_from_vertex[source].extend(edges)
-        self.n_edges += len(edges)
 
     def get_edges_from(self, vertex : int):
         return self.edges_from_vertex[vertex]
